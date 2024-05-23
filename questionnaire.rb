b05a1a08e0e43b7d21c8d90846a33a3ac3fb95e6
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-05-23 20:47:16 +0530

    Code comments added

--- questionnaire.rb
+++ questionnaire.rb
@@ -1,5 +1,7 @@
+# Import pstore to persist the data
 require "pstore" # https://github.com/ruby/pstore
 
+# Constants
 STORE_NAME = "tendable.pstore"
 
 QUESTIONS = {
@@ -10,31 +12,34 @@ QUESTIONS = {
   "q5" => "Can you code in C#?"
 }.freeze
 
+# Method to prompt user for answer(s) to questions
 def do_prompt
   answers = {}
   QUESTIONS.each do |question_key, question|
-    puts "#{question} (Yes/No): "
-    answer = gets.chomp.downcase
-    answers[question_key] = answer == "yes" || answer == "y" ? true : false
+    puts "#{question} (Yes/No): "  # Prompt user for input
+    answer = gets.chomp.downcase # Get input and convert to lowercase
+    answers[question_key] = answer == "yes" || answer == "y" ? true : false # Store answers as true or false
   end
   answers
 end
 
+# Method to calculate and print the Rating
 def do_report(answers)
   total_questions = QUESTIONS.size
   store = PStore.new(STORE_NAME)
-  yes_total = answers.count{|_key, val| val }
-  current_rating = yes_total.to_f / total_questions * 100
+  yes_total = answers.count{|_key, val| val } #Count the total number of 'Yes' answers
+  current_rating = yes_total.to_f / total_questions * 100 # Calculate rating
   puts "Your Rating for this Run : #{current_rating.round(2)}"
   store.transaction do 
     store[:ratings] ||=[]
     store[:ratings] << current_rating
   end
-  avg_rating = store.transaction{ (store[:ratings].sum.to_f / store[:ratings].size).round(2) }
+  avg_rating = store.transaction{ (store[:ratings].sum.to_f / store[:ratings].size).round(2) } # Calculate average rating
   puts "Average Rating for all Run: #{avg_rating}%"
   current_rating
 end
 
+# Method to instantiate the Survey
 def run_survey
   loop do
     p "Welcome to the Coding Survey !"
